b726cdd4cbe747cbcdb083ec19a473682ef8bc28
서가희 <kahee430@gmail.com>
2024-01-26 20:38:08 +0900

    counter만들기 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,51 @@
+// 13강, 14강(counter만들기) MVC모델 느낌?
+
+// Redux redux, modules 폴더, memo,파일 참고
+
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+// store : state들의 집합 저장소, 데이터 관리소
 
 function App() {
-  return <div>App</div>;
+  // useSelector
+  // 여기에 store에 접근하여 counter의 값을 읽어오기 위해 사용
+  // 매개변수 state는 store에 저장된 state이다
+  const counter = useSelector(function (state) {
+    return state.counter;
+    // state의 counter를 반환
+  });
+  console.log("counter", counter.number);
+
+  // action을 보내는 dispatch
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <div>현재 카운트 : {counter.number}</div>
+      <button
+        onClick={() => {
+          // counter가 +1을 해주는 로직 작성
+          dispatch({
+            type: "PLUS_ONE",
+            // 모듈 리듀스 액션 타입을 넣어줌
+          });
+          // dispatch 안에 액션 객체를 넣어줘야한다 type,payload
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          dispatch({
+            type: "MINUS_ONE",
+          });
+        }}
+      >
+        -
+      </button>
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./redux/config/configStore";
+
 // import "./Reset.css"; 스타일 리셋
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+  // redux강의 / Provider는 스테이트의 집합인 스토어를 기반으로 지배권을 행사하기 위해 App 컴포넌트를 감싸야 스토어로 만들어 놓은 중앙데이터 관리소를 app컴포넌트 아래에서 사용 할 수 있다
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
